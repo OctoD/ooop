63c818586f16c847f313f0f397a7fb00d6588153
test <p.roth@orange-its.ch>
2019-04-15 14:38:51 +0200

    chore: adds exports

--- src/errors.ts
+++ src/errors.ts
@@ -1,2 +1,6 @@
 export * from './Errors/InvalidCastError';
+export * from './Errors/InvalidLeftOperatorType';
+export * from './Errors/InvalidRightOperatorType';
 export * from './Errors/NullPointerError';
+export * from './Errors/OperationError';
+export * from './Errors/OutOfRangeError';
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,12 @@
 import ObjectArray from './Types/ObjectArray';
 import ObjectBit from './Types/ObjectBit';
+import ObjectBitArray from './Types/ObjectBitArray';
 import ObjectBoolean from './Types/ObjectBoolean';
+import ObjectByte from './Types/ObjectByte';
 import ObjectChar from './Types/ObjectChar';
 import ObjectDecimal from './Types/ObjectDecimal';
+import ObjectFloat from './Types/ObjectFloat';
+import ObjectFunction from './Types/ObjectFunction';
 import ObjectInt from './Types/ObjectInt';
 import ObjectNull from './Types/ObjectNull';
 import ObjectObject from './Types/ObjectObject';
@@ -14,9 +18,13 @@ import TypeBase from './Types/TypeBase';
 export {
   ObjectArray,
   ObjectBit,
+  ObjectBitArray,
   ObjectBoolean,
+  ObjectByte,
   ObjectChar,
   ObjectDecimal,
+  ObjectFloat,
+  ObjectFunction,
   ObjectInt,
   ObjectNull,
   ObjectObject,
